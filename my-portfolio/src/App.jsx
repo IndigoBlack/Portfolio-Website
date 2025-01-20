ebc868c2653b9fa4c9618f82c0123390a31488fc
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2025-01-20 14:16:50 +0000

    editing

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,5 +1,5 @@
-// Create a React portfolio Website
-// The website must have the pages Home, about, Contact Me, and a C.V 
+// Create a React portfolio Website 
+// The website must have the pages Home, about, Contact Me, Projects, and a C.V 
 import React from 'react';
 import Home from './Home';
 import About from './About';
