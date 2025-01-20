65e78c34bca9bd7913e856f2f74a54867b080f85
Mamello Molokwane <161704049+IndigoBlack@users.noreply.github.com>
2025-01-20 14:48:04 +0000

    Added comments for the project section

--- my-portfolio/src/Projects.jsx
+++ my-portfolio/src/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Projects() {
+    // Create the project in an object and display the objects using the map function
     return (
         <>
             <h2>Projects</h2>
